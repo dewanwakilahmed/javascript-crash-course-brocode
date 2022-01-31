ea2c118ed046a6013523040481f72e64bf634e97
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-31 21:48:44 +0600

    Variables

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-// alert("I love pizza!");
-console.log("I really really enjoy pizza!");
+var age = 21;
+var firstName = "Bro";
+var lastName = "Code";
+var online = true;
+var whatever;
+
+console.log("Your age is " + age);
+console.log("Your age is", age);
+age = age + 1;
+console.log("Your age is", age);
+
+console.log("Hello", firstName);
+
+console.log("Are you online?:", online);
+
+console.log(whatever);
+whatever = "something";
+console.log(whatever);
+whatever = null;
+console.log(whatever);
+
+var fullName = firstName + " " + lastName;
+console.log("Hello", fullName);
+
+let mySweetVariable = 420; // temporary assign a value
+const pi = 3.14159;
+
+console.log(pi); // Can't be reassigned
