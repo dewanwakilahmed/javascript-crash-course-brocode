752a2aa7fabc17bf0292d3cf890f45d3d0dcc024
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-31 22:53:23 +0600

    Type Conversion

--- userInput.js
+++ userInput.js
@@ -2,7 +2,37 @@
 
 // console.log("Hello", myName);
 
-document.getElementById("myButton").onclick = function () {
-  var myName = document.getElementById("myText").value;
-  console.log("Hello", myName);
-};
+// document.getElementById("myButton").onclick = function () {
+//   var myName = document.getElementById("myText").value;
+//   console.log("Hello", myName);
+// };
+
+// var age = window.prompt("What is your age?");
+// console.log(typeof age);
+// console.log("Happy Birthday!");
+// age = Number(age); // Explicit Type Conversion
+// console.log(typeof age);
+// age += 1;
+// console.log(age);
+
+// age = String(age);
+// console.log(typeof age);
+// age += 1;
+// console.log(age);
+
+var myBool;
+
+boolVar = Boolean(0);
+console.log(boolVar);
+
+boolVar = Boolean("");
+console.log(boolVar);
+
+boolVar = Boolean(null);
+console.log(boolVar);
+
+boolVar = Boolean(undefined);
+console.log(boolVar);
+
+boolVar = Boolean(NaN);
+console.log(boolVar);
