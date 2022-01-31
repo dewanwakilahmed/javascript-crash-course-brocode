63336265c179abff8646f83aad67827e41d1cd46
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-31 22:21:56 +0600

    Arithmetic Expressions

---
+++ arithmetic.js
@@ -0,0 +1,29 @@
+var friends = 10;
+console.log("Intially:", friends);
+
+// friends = friends + 1;
+friends += 1;
+console.log("After adding 1:", friends);
+
+// friends = friends - 1;
+friends -= 1;
+console.log("After substracting 1:", friends);
+
+// friends = friends * 2;
+friends *= 2;
+console.log("After multiplying with 2:", friends);
+
+// friends = friends / 4;
+friends /= 4;
+console.log("After dividing by 4:", friends);
+
+// friends = friends ** 2;
+friends **= 2;
+console.log("After raising by power 2:", friends);
+
+// friends = friends % 3;
+friends %= 3;
+console.log("Modulus of 3:", friends);
+
+friends = 10 + 2 / 2;
+console.log(friends);
